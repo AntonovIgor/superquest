45ca095b72b9db33f423df679500cda05a1f0a6a
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-10-16 19:50:34 +0300

    Добавляет работу с адресной строкой

--- js/application.js
+++ js/application.js
@@ -4,16 +4,56 @@ import {initialGame} from './data/quest';
 import GameOverScreen from './gameover/gameover';
 import scoreboard from './scoreboard/scoreboard';
 
-const winScreen = scoreboard;
 const dieScreen = new GameOverScreen(false);
 
+const ControllerId = {
+  WELCOME: ``,
+  GAME: `game`,
+  SCORE: `score`
+};
+
+const saveState = (state) => {
+  return JSON.stringify(state);
+};
+
+const loadState = (dataString) => {
+  try {
+    return JSON.parse(dataString);
+  } catch (e) {
+    return initialGame;
+  }
+};
+
+const routes = {
+  [ControllerId.WELCOME]: welcomeScreen,
+  [ControllerId.GAME]: gameScreen,
+  [ControllerId.SCORE]: scoreboard
+};
+
 export default class Application {
+  static init() {
+    const hashChangeHandler = () => {
+      const hashValue = location.hash.replace(`#`, ``);
+      const [id, data] = hashValue.split(`?`);
+      this.changeHash(id, data);
+    };
+    window.onhashchange = hashChangeHandler;
+    hashChangeHandler();
+  }
+
+  static changeHash(id, data) {
+    const controller = routes[id];
+    if (controller) {
+      controller.init(loadState(data));
+    }
+  }
+
   static showWelcome() {
-    welcomeScreen.init();
+    location.hash = ControllerId.WELCOME;
   }
 
   static startGame(state = initialGame) {
-    gameScreen.init(state);
+    location.hash = `${ControllerId.GAME}?${saveState(state)}`;
   }
 
   static die(state) {
@@ -21,6 +61,8 @@ export default class Application {
   }
 
   static win(state) {
-    winScreen.init(state);
+    location.hash = `${ControllerId.SCORE}?${saveState(state)}`;
   }
 }
+
+Application.init();
