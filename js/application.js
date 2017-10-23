73067f0f6baca1fc1dd356065fe2ad4d6bcc63cf
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-10-24 00:20:36 +0300

    Добавляет таблицу результатов Убирает загрузку состояния из адресной строки

--- js/application.js
+++ js/application.js
@@ -1,12 +1,12 @@
 import welcomeScreen from './welcome/welcome';
 import GameScreen from './game/game';
-import { initialGame } from './data/quest';
+import {initialGame} from './data/quest';
 import GameOverScreen from './gameover/gameover';
 import scoreboard from './scoreboard/scoreboard';
 import Loader from './loader';
 import SplashScreen from './splash/splash-screen';
 import adapt from './data/quest-adapter';
-import { changeView } from './util';
+import {changeView} from './util';
 
 const dieScreen = new GameOverScreen(false);
 
@@ -14,18 +14,6 @@ const ControllerId = {
   WELCOME: ``,
   GAME: `game`,
   SCORE: `score`
-};
-
-const saveState = (state) => {
-  return JSON.stringify(state);
-};
-
-const loadState = (dataString) => {
-  try {
-    return JSON.parse(dataString);
-  } catch (e) {
-    return initialGame;
-  }
 };
 
 export default class Application {
@@ -38,17 +26,16 @@ export default class Application {
 
     const hashChangeHandler = () => {
       const hashValue = location.hash.replace(`#`, ``);
-      const [id, data] = hashValue.split(`?`);
-      Application.changeHash(id, data);
+      Application.changeHash(hashValue);
     };
     window.onhashchange = hashChangeHandler;
     hashChangeHandler();
   }
 
-  static changeHash(id, data) {
+  static changeHash(id) {
     const controller = Application.routes[id];
     if (controller) {
-      controller.init(loadState(data));
+      controller.init();
     }
   }
 
